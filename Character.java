43cc44fc67b3ef888b793f1bfe16663a34c200c4
Rsotaing Adrien <80404101+AdrienRostaing@users.noreply.github.com>
2023-03-24 06:13:05 +0100

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,4 +1,20 @@
 package Game;
 
-public class Character {
+public abstract class Character {
+
+    //Variables / Attributes all characters have
+    public String name;
+    public int maxHP, hp, xp;
+
+    // Constructor for character
+    public Character(String name, int maxHP, int xp){
+        this.name = name;
+        this.maxHP = maxHP;
+        this.xp = xp;
+        this.hp = maxHP;
+    }
+
+    //methods every character has
+    public abstract int attack();
+    public abstract int defend();
 }
